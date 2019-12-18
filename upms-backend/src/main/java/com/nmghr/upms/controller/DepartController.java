f9391fae72e00873b9fb0b0cd9c3ca918370dda3
liubo <Lb@123>
2019-12-18 10:54:08 +0800

    队伍统计

--- upms-backend/src/main/java/com/nmghr/upms/controller/DepartController.java
+++ upms-backend/src/main/java/com/nmghr/upms/controller/DepartController.java
@@ -28,6 +28,32 @@ public class DepartController {
   public Object subordinate(String areaCode, int curType) throws Exception {
     return departService.subordinate(areaCode, curType);
   }
+  /**
+   * 队伍信息统计
+   *
+   * @return
+   */
+  @GetMapping(value = "/statistics")
+  public Object statistics(String provinceCode, String cityCode, String departCode, String reginCode, String userSort) throws Exception {
+    return departService.statisticsList(provinceCode, cityCode, reginCode, departCode, userSort);
+  }
+  /**
+   * 队伍信息统计 图形统计
+   *
+   * @return
+   */
+  @GetMapping(value = "/statisticsChart")
+  public Object statisticsChart(String provinceCode, String cityCode, String departCode, String reginCode, String userSort) throws Exception {
+    return departService.statisticsEchart(provinceCode, cityCode, reginCode, departCode, userSort);
+  }
 
-
+  /**
+   * 查询某队伍详细信息
+   * @return
+   * @throws Exception
+   */
+  @GetMapping(value = "/statisticsDetail")
+  public Object statisticsDetail(String provinceCode, String cityCode, String departCode, String reginCode, String userSort) throws Exception {
+    return departService.statisticsDetail(provinceCode, cityCode, reginCode, departCode, userSort);
+  }
 }
